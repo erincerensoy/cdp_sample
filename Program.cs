d5bf1adab493fbe5d3a93580df5c393c9d8fd2ac
02484220 <02484220@02484220-N2>
2018-10-09 00:15:55 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cdp_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
